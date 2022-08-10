b33a24a338e174c3f7b8ed14e3dc9b69b0d8d02a
Ashish Bisht <ashish12345bisht@gmail.com>
2022-08-10 10:16:30 +0530

    api working-CORS error resolved

--- nyka_backend/index.js
+++ nyka_backend/index.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 const userRoutes = require("./controllers/userController");
 const mongoose = require("mongoose");
+var cors = require("cors");
 app.use(express.json());
-app.use("/", userRoutes);
 
 const mongoURI = "mongodb://localhost:27017/nyka";
 mongoose
@@ -18,6 +18,9 @@ mongoose
   .catch((err) => {
     console.log("error ", err);
   });
+
+app.use(cors());
+app.use("/", userRoutes);
 app.listen(process.env.PORT, () => {
   console.log("Server Running at port ", process.env.PORT);
 });
